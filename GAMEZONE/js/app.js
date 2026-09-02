2e9b83923fb2e8ae8c72ddec54c1ec1b7b19d7d2
maurosantiago23 <mau.santiago@duocuc.cl>
2026-09-02 15:41:15 -0400

    contenido html y js

---
+++ GAMEZONE/js/app.js
@@ -0,0 +1,8 @@
+console.log("JavaScript funcionando desde app.js");
+
+const titulo = document.getElementById("titulo-sitio");
+
+console.log(titulo);
+
+titulo.textContent = "¡Bienvenido a GAMEZONE!";
+
